75991caecaedae35e168cac005a33e7ceda50c13
CarlosEReis <carlos.er7@gmail.com>
2023-12-07 14:21:43 -0300

    03.11. Atualizando um objeto no banco de dados

---
+++ src/main/java/com/er7/er7foodapi/jpa/AlteracaoCozinhaMain.java
@@ -0,0 +1,26 @@
+package com.er7.er7foodapi.jpa;
+
+import com.er7.er7foodapi.Er7foodApiApplication;
+import com.er7.er7foodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class AlteracaoCozinhaMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext  = new SpringApplicationBuilder(Er7foodApiApplication.class)
+            .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
+
+        cozinha = cadastroCozinha.adicionar(cozinha);
+
+    }
+}
