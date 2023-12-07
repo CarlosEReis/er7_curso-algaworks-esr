7588e73c8fd2abf8c791afa5e95e1ed0647ae9ae
CarlosEReis <carlos.er7@gmail.com>
2023-12-07 10:25:29 -0300

    03.03: Adicionando JPA e configurando o Data Source

---
+++ src/main/java/com/er7/er7foodapi/Er7foodApiApplication.java
@@ -0,0 +1,13 @@
+package com.er7.er7foodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Er7foodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Er7foodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/er7/er7foodapi/Er7foodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.er7.er7foodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Er7foodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
