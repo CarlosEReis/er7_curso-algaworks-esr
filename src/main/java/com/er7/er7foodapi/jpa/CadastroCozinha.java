2422d6b96639fe7f32cca50e80611529b4af22b5
CarlosEReis <carlos.er7@gmail.com>
2023-12-07 14:57:55 -0300

    03.12: Excluindo um objeto do banco de dados

--- src/main/java/com/er7/er7foodapi/jpa/CadastroCozinha.java
+++ src/main/java/com/er7/er7foodapi/jpa/CadastroCozinha.java
@@ -27,5 +27,9 @@ public class CadastroCozinha {
         return manager.merge(cozinha);
     }
 
-
+    @Transactional
+    public void remover(Cozinha cozinha) {
+        cozinha = buscar(cozinha.getId());
+        manager.remove(cozinha);
+    }
 }
