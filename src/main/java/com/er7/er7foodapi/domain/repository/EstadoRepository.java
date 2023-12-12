f3f35a50c15036a9d6baeedc53a7ad1e10a5f108
CarlosEReis <carlos.er7@gmail.com>
2023-12-12 18:53:33 -0300

    04.11: Desafio: collection resource de estados

--- src/main/java/com/er7/er7foodapi/domain/repository/EstadoRepository.java
+++ src/main/java/com/er7/er7foodapi/domain/repository/EstadoRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface EstadoRepository {
 
-    List<Estado> buscar();
+    List<Estado> listar();
     Estado buscar(Long id);
     Estado salvar(Estado estado);
     void remover(Estado estado);
