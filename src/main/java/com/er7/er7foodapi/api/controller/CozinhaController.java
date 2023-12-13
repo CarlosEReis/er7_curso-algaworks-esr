3dd0427f6ef670d6419f73dfc91c0b9a55f5c9b4
CarlosEReis <carlos.er7@gmail.com>
2023-12-13 08:23:52 -0300

    04.13: add content negotiation p retornar JSON ou XML

--- src/main/java/com/er7/er7foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/er7/er7foodapi/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.er7.er7foodapi.api.controller;
 import com.er7.er7foodapi.domain.model.Cozinha;
 import com.er7.er7foodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,15 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
-    public List<Cozinha> listar() {
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
+    public List<Cozinha> listar1() {
+        System.out.println("PRODUCES >>>> JSON");
+        return this.cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = { MediaType.APPLICATION_XML_VALUE })
+    public List<Cozinha> listar2() {
+        System.out.println("PRODUCES >>>> XML");
         return this.cozinhaRepository.listar();
     }
 }
