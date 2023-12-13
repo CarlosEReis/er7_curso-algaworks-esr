3e8106c85a2e47c79c523706820afa3f9eebb416
CarlosEReis <carlos.er7@gmail.com>
2023-12-13 08:47:04 -0300

    04.14: Consultando Singleton Resource com GET e @PathVariable

--- src/main/java/com/er7/er7foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/er7/er7foodapi/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.er7.er7foodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,15 +18,13 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
-    public List<Cozinha> listar1() {
-        System.out.println("PRODUCES >>>> JSON");
+    @GetMapping
+    public List<Cozinha> listar() {
         return this.cozinhaRepository.listar();
     }
 
-    @GetMapping(produces = { MediaType.APPLICATION_XML_VALUE })
-    public List<Cozinha> listar2() {
-        System.out.println("PRODUCES >>>> XML");
-        return this.cozinhaRepository.listar();
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return this.cozinhaRepository.buscar(cozinhaId);
     }
 }
