67ceae0ed951ff2a84ae49191d910156da453b17
CarlosEReis <carlos.er7@gmail.com>
2023-12-15 17:50:05 -0300

    04.34: Finalizando a atualização parcial com a API de Reflections do Spring

--- src/main/java/com/er7/er7foodapi/api/controller/RestauranteController.java
+++ src/main/java/com/er7/er7foodapi/api/controller/RestauranteController.java
@@ -4,12 +4,15 @@ import com.er7.er7foodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.er7.er7foodapi.domain.model.Restaurante;
 import com.er7.er7foodapi.domain.repository.RestauranteRepository;
 import com.er7.er7foodapi.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
@@ -67,6 +70,13 @@ public class RestauranteController {
     }
 
     private void merge(Map<String, Object> campos, Restaurante restauranteDestino) {
-        campos.forEach((campo, valor) -> System.out.println(campo + " = " + valor) );
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante restauranteOrigem = objectMapper.convertValue(campos, Restaurante.class);
+        campos.forEach((campo, valor) -> {
+            Field field = ReflectionUtils.findField(Restaurante.class, campo);
+            field.setAccessible(true);
+            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+            ReflectionUtils.setField(field, restauranteDestino, novoValor);
+        } );
     }
 }
