47c184028236143c26ff84538bed445fe34383fa
CarlosEReis <carlos.er7@gmail.com>
2023-12-19 17:19:27 -0300

    06.01: Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/er7/er7foodapi/domain/model/Cozinha.java
+++ src/main/java/com/er7/er7foodapi/domain/model/Cozinha.java
@@ -1,13 +1,13 @@
 package com.er7.er7foodapi.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 @JsonRootName("cozinha")
 @Data @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -22,4 +22,7 @@ public class Cozinha {
     @Column(nullable = false)
     private String nome;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<>();
 }
