7b495b089ff54a6b44a8ac6cbe3434ab18b455d5
CarlosEReis <carlos.er7@gmail.com>
2023-12-29 10:49:32 -0300

    08.02: Lançando exceções customizadas anotadas com @ResponseStatus

--- src/main/java/com/er7/er7foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/er7/er7foodapi/api/controller/CozinhaController.java
@@ -53,12 +53,17 @@ public class CozinhaController {
     }
 
     @DeleteMapping("/{cozinhaId}")
-    public ResponseEntity<?> remover(@PathVariable Long cozinhaId) {
-        try {
-            this.cadastroCozinha.excluir(cozinhaId);
-            return ResponseEntity.noContent().build();
-        } catch (EntidadeNaoEncontradaException e) { return ResponseEntity.notFound().build();
-        } catch (EntidadeEmUsoException e) { return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage()); }
-
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long cozinhaId) {
+        this.cadastroCozinha.excluir(cozinhaId);
     }
+
+//    @DeleteMapping("/{cozinhaId}")
+//    public ResponseEntity<?> remover(@PathVariable Long cozinhaId) {
+//        try {
+//            this.cadastroCozinha.excluir(cozinhaId);
+//            return ResponseEntity.noContent().build();
+//        } catch (EntidadeNaoEncontradaException e) { return ResponseEntity.notFound().build();
+//        } catch (EntidadeEmUsoException e) { return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage()); }
+//    }
 }
