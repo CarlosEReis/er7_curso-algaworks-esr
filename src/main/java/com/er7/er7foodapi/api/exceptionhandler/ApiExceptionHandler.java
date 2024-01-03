784ac5dc80520c9fb48acceddfffc9642484cacb
CarlosEReis <carlos.er7@gmail.com>
2024-01-03 08:09:30 -0300

    08.25: Desafio: tratando exception de parâmetro de URL inválido

--- src/main/java/com/er7/er7foodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/er7/er7foodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -7,13 +7,16 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
@@ -23,42 +26,24 @@ import java.util.stream.Collectors;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        if (ex instanceof MethodArgumentTypeMismatchException) {
+            return handleMethodArgumentTypeMismatch((MethodArgumentTypeMismatchException) ex, headers, status, request);
+        }
+        return super.handleTypeMismatch(ex, headers, status, request);
+    }
+
+    @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Throwable rootCause = ExceptionUtils.getRootCause(ex);
         if (rootCause instanceof InvalidFormatException) {
-            return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
+            return handleInvalidFormat((InvalidFormatException) rootCause, headers, status, request);
         } else if (rootCause instanceof PropertyBindingException) {
-            return handlePropertyBindingException((PropertyBindingException) rootCause, headers, status, request);
+            return handlePropertyBinding((PropertyBindingException) rootCause, headers, status, request);
         }
         ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
         String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
         var problem = createProblemBuilder(status, problemType, detail).build();
-        return handleExceptionInternal(ex, problem, headers, status, request);
-    }
-
-
-    private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        var path = joinPath(ex.getPath());
-        var problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
-        var detail = String.format("A propriedade '%s' não existe. Remova a propriedade e tente novamente.", path);
-        var problem = createProblemBuilder(status, problemType, detail).build();
-        return handleExceptionInternal(ex, problem, headers, status, request);
-    }
-
-    private static String joinPath(List<JsonMappingException.Reference> ex) {
-        return ex.stream()
-                .map(ref -> ref.getFieldName())
-                .collect(Collectors.joining("."));
-    }
-
-    private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        var path = joinPath(ex.getPath());
-
-        System.out.println(path);
-        var problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
-        var detail = String.format("A propriedade '%s' recebeu o valor '%s', que é de um tipo inválido. Corrija e informe o valor do tipo '%s'", path, ex.getValue(), ex.getTargetType().getSimpleName());
-        var problem = createProblemBuilder(status, problemType, detail).build();
-
         return handleExceptionInternal(ex, problem, headers, status, request);
     }
 
@@ -105,11 +90,46 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 
+
     private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
         return Problem.builder()
                 .status(status.value())
                 .type(problemType.getUri())
                 .title(problemType.getTitle())
                 .detail(detail);
+    }
+
+    private ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        var problemType = ProblemType.PARAMETRO_INVALIDO;
+        var detail = String.format(
+                "O parâmetro de URL '%s' recebeu o valor '%s', que é um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
+                ex.getName(),
+                ex.getValue(),
+                ex.getParameter().getParameterType().getSimpleName());
+        var problem = createProblemBuilder(status, problemType, detail).build();
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
+    private ResponseEntity<Object> handlePropertyBinding(PropertyBindingException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        var path = joinPath(ex.getPath());
+        var problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+        var detail = String.format("A propriedade '%s' não existe. Remova a propriedade e tente novamente.", path);
+        var problem = createProblemBuilder(status, problemType, detail).build();
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
+    private static String joinPath(List<JsonMappingException.Reference> ex) {
+        return ex.stream()
+                .map(ref -> ref.getFieldName())
+                .collect(Collectors.joining("."));
+    }
+
+    private ResponseEntity<Object> handleInvalidFormat(InvalidFormatException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        var path = joinPath(ex.getPath());
+        System.out.println(path);
+        var problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+        var detail = String.format("A propriedade '%s' recebeu o valor '%s', que é de um tipo inválido. Corrija e informe o valor do tipo '%s'", path, ex.getValue(), ex.getTargetType().getSimpleName());
+        var problem = createProblemBuilder(status, problemType, detail).build();
+        return handleExceptionInternal(ex, problem, headers, status, request);
     }
 }
