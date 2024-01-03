ac8dbcbcc7b5d9d91540c62117fb8ee0fc5319e6
CarlosEReis <carlos.er7@gmail.com>
2024-01-03 16:59:42 -0300

    08.27: Desafio: tratando outras exceções não capturadas

--- src/main/java/com/er7/er7foodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/er7/er7foodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -25,6 +25,16 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
+        var status = HttpStatus.INTERNAL_SERVER_ERROR;
+        var problemType = ProblemType.ERRO_DE_SISTEMA;
+        var detail = "Ocorreu um erro inesperado no sistema. Tente novamente, e se o problema persistir, entre em contato com o administrador do sistema.";
+        ex.getStackTrace();
+        var problem = createProblemBuilder(status, problemType, detail).build();
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
     @Override
     protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         var problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
