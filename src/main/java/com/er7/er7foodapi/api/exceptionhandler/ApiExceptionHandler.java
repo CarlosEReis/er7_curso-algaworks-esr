402fe2fe5e52fb135c7b4f7f7cdcd87458298151
CarlosEReis <carlos.er7@gmail.com>
2024-01-09 08:45:52 -0300

    09.03: Desafio: tratando exception de violação de constraints de validação

--- src/main/java/com/er7/er7foodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/er7/er7foodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -39,6 +40,16 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 .userMessage(detail)
                 .build();
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        var problemType = ProblemType.DADOS_INVALIDOS;
+        var detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
+        var probem = createProblemBuilder(status, problemType, detail)
+                .userMessage(detail)
+                .build();
+        return handleExceptionInternal(ex, probem, new HttpHeaders(), status, request);
     }
 
     @Override
