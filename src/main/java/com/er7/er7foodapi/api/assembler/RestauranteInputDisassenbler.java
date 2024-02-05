b0426e64b80d4e0de9cd4ae506962719ad20edaf
CarlosEReis <carlos.er7@gmail.com>
2024-02-05 15:23:41 -0300

    11.14: Adicionando e usando o ModelMapper

--- src/main/java/com/er7/er7foodapi/api/assembler/RestauranteInputDisassenbler.java
+++ src/main/java/com/er7/er7foodapi/api/assembler/RestauranteInputDisassenbler.java
@@ -1,22 +1,18 @@
 package com.er7.er7foodapi.api.assembler;
 
 import com.er7.er7foodapi.api.model.input.RestauranteInput;
-import com.er7.er7foodapi.domain.model.Cozinha;
 import com.er7.er7foodapi.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestauranteInputDisassenbler {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-
-        restaurante.setCozinha(cozinha);
-        return restaurante;
+        return modelMapper.map(restauranteInput, Restaurante.class);
     }
 }
