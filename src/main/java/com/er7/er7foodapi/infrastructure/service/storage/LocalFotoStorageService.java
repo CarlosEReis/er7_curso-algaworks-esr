ee7c728e6585f07826a9895c1636b9e4f2d0fe60
CarlosEReis <carlos.er7@gmail.com>
2024-03-10 16:52:27 -0300

    14.08: Impl o serviço de armazenagem de fotos no disco local

---
+++ src/main/java/com/er7/er7foodapi/infrastructure/service/storage/LocalFotoStorageService.java
@@ -0,0 +1,31 @@
+package com.er7.er7foodapi.infrastructure.service.storage;
+
+import com.er7.er7foodapi.domain.service.FotoStorageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class LocalFotoStorageService implements FotoStorageService {
+
+    @Value("${er7food.storage.local.diretorio-fotos}")
+    private Path diretorioFotos;
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+        try {
+            Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
+            FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
+        } catch (IOException e) {
+            throw new StorageException("Não foi possível armazenar o arquivo.", e);
+        }
+    }
+
+    private Path getArquivoPath(String nomeArquivo) {
+        return diretorioFotos.resolve(Path.of(nomeArquivo));
+    }
+}
