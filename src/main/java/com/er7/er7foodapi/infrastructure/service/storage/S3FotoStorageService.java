b2af9af09e9d95ad4fe9c01c5d5fb90aaf05140c
CarlosEReis <carlos.er7@gmail.com>
2024-03-22 20:01:22 -0300

    14.22: Def bean do client da Amazon S3 e config credenciais

--- src/main/java/com/er7/er7foodapi/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/er7/er7foodapi/infrastructure/service/storage/S3FotoStorageService.java
@@ -1,13 +1,16 @@
 package com.er7.er7foodapi.infrastructure.service.storage;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.er7.er7foodapi.domain.service.FotoStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.InputStream;
 
 //@Service
 public class S3FotoStorageService implements FotoStorageService {
 
-
+    @Autowired
+    private AmazonS3 amazonS3;
 
     @Override
     public InputStream recuperar(String nomeArquivo) {
