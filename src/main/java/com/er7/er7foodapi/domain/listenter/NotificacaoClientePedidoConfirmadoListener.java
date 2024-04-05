f6fde89c94338bbcb953a38611fef150b89714d1
CarlosEReis <carlos.er7@gmail.com>
2024-04-05 19:33:24 -0300

    15.13: Reagindo a Domain Events em fases específicas da transação

--- src/main/java/com/er7/er7foodapi/domain/listenter/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/er7/er7foodapi/domain/listenter/NotificacaoClientePedidoConfirmadoListener.java
@@ -5,15 +5,15 @@ import com.er7.er7foodapi.domain.model.Pedido;
 import com.er7.er7foodapi.domain.service.EnvioEmailService;
 import com.er7.er7foodapi.domain.service.EnvioEmailService.Mensagem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class NotificacaoClientePedidoConfirmadoListener {
 
     @Autowired private EnvioEmailService envioEmail;
 
-    @EventListener
+    @TransactionalEventListener
     public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
         Pedido pedido = event.getPedido();
         Mensagem menssagem = Mensagem.builder()
