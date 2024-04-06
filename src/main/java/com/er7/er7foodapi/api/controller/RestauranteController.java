78edef619edb3700934045e34eddd5c72bbbf924
CarlosEReis <carlos.er7@gmail.com>
2024-04-06 09:50:26 -0300

    16.04: Habilitando CORS em controladores e métodos com @CrossOrigin

--- src/main/java/com/er7/er7foodapi/api/controller/RestauranteController.java
+++ src/main/java/com/er7/er7foodapi/api/controller/RestauranteController.java
@@ -18,9 +18,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
@@ -34,6 +32,7 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://192.168.1.5:8000")
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
@@ -47,19 +46,15 @@ public class RestauranteController {
 
     @JsonView(RestauranteView.Resumo.class)
     @GetMapping
-    public ResponseEntity<List<RestauranteModel>> listar() {
-        List<RestauranteModel> restaurantesModels = restauranteModelAssembler.toCollectionModel(this.restauranteRepository.findAll());
-        return ResponseEntity
-            .ok()
-            .header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://192.168.1.5:8000")
-            .body(restaurantesModels);
+    public List<RestauranteModel> listar() {
+        return restauranteModelAssembler.toCollectionModel(this.restauranteRepository.findAll());
     }
 
-    //@JsonView(RestauranteView.ApenasNome.class)
-    //@GetMapping(params = "projecao=apenas-nome")
-    //public List<RestauranteModel> listarApenasNomes() {
-    //    return listar();
-    // }
+    @JsonView(RestauranteView.ApenasNome.class)
+    @GetMapping(params = "projecao=apenas-nome")
+    public List<RestauranteModel> listarApenasNomes() {
+        return listar();
+    }
 
 //    @GetMapping
 //    public MappingJacksonValue listar(@RequestParam(required = false) String projecao) {
