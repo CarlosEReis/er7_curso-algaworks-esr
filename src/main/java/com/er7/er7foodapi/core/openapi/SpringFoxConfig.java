c4c5841c77ca81a76c2ede79e00d5d176163d479
CarlosEReis <carlos.er7@gmail.com>
2024-04-15 18:36:57 -0300

    18.12: Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/er7/er7foodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/er7/er7foodapi/core/openapi/SpringFoxConfig.java
@@ -3,14 +3,21 @@ package com.er7.er7foodapi.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
@@ -23,11 +30,30 @@ public class SpringFoxConfig {
             .apis(
                 RequestHandlerSelectors.basePackage("com.er7.er7foodapi.api"))
             .build()
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, globalGetResponseMessages())
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades."));
     }
 
-    public ApiInfo apiInfo() {
+    private List<Response> globalGetResponseMessages(){
+        return Arrays.asList(
+            new ResponseBuilder()
+                .code(String.valueOf(HttpStatus.OK.value()))
+                .description("Consulta realizada com sucesso.")
+                .build(),
+            new ResponseBuilder()
+                .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                .description("Erro interno do servidor")
+                .build(),
+            new ResponseBuilder()
+                .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                .description("Recurso não possui representação que poderia ser aceita pelo consumidor.")
+                .build()
+        );
+    }
+
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
             .title("ER7Food API")
             .description("API aberta para clientes e restaurantes.")
