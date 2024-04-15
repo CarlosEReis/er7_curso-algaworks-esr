0107db0a0b6f9e6c73d4636fef828454da3a5a67
CarlosEReis <carlos.er7@gmail.com>
2024-04-15 00:44:09 -0300

    18.10. Descrevendo modelos de representações e suas propriedades

--- src/main/java/com/er7/er7foodapi/api/model/CidadeModel.java
+++ src/main/java/com/er7/er7foodapi/api/model/CidadeModel.java
@@ -1,13 +1,19 @@
 package com.er7.er7foodapi.api.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeModel {
 
+    @ApiModelProperty(value = "ID da cidade", example = "1")
     private Long id;
+
+    @ApiModelProperty(value = "Nome da cidade", example = "Uberlândia")
     private String nome;
     private EstadoModel estado;
 }
--- src/main/java/com/er7/er7foodapi/api/model/EstadoModel.java
+++ src/main/java/com/er7/er7foodapi/api/model/EstadoModel.java
@@ -1,5 +1,6 @@
 package com.er7.er7foodapi.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,9 @@ import lombok.Setter;
 @Setter
 public class EstadoModel {
 
+    @ApiModelProperty(value = "ID do estado", example = "1")
     private Long id;
+
+    @ApiModelProperty(value = "Nome do estado", example = "Minas Gerais")
     private String nome;
 }
