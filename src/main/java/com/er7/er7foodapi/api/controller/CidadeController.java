f3632582f6ab58bf383142e427d5511628b499f2
CarlosEReis <carlos.er7@gmail.com>
2024-04-15 21:58:13 -0300

    18.16: Descrevendo códigos de status de respostas em endpoints específicos

--- src/main/java/com/er7/er7foodapi/api/controller/CidadeController.java
+++ src/main/java/com/er7/er7foodapi/api/controller/CidadeController.java
@@ -2,6 +2,7 @@ package com.er7.er7foodapi.api.controller;
 
 import com.er7.er7foodapi.api.assembler.CidadeInputDisassembler;
 import com.er7.er7foodapi.api.assembler.CidadeModelAssembler;
+import com.er7.er7foodapi.api.exceptionhandler.Problem;
 import com.er7.er7foodapi.api.model.CidadeModel;
 import com.er7.er7foodapi.api.model.input.CidadeInput;
 import com.er7.er7foodapi.domain.exception.EstadoNaoEncontradoException;
@@ -11,6 +12,10 @@ import com.er7.er7foodapi.domain.service.CadastroCidadeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -35,12 +40,20 @@ public class CidadeController {
     }
 
     @ApiOperation("Busca uma cidade específica com base no ID.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+            content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class))),
+        @ApiResponse(responseCode = "400", description = "ID da cidade é inválido",
+            content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class)))
+    })
     @GetMapping("/{cidadeId}")
     public CidadeModel buscar(@ApiParam(value = "ID de uma cidade") @PathVariable Long cidadeId) {
         return cidadeModelAssembler.toModel(cidadeService.buscarOuFalhar(cidadeId));
     }
 
     @ApiOperation("Cadastra uma cidade.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Cidade cadastrada")})
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CidadeModel adicionar(@ApiParam(name = "corpo", value = "Representação de uma nova cidade") @RequestBody @Valid CidadeInput cidadeInput) {
@@ -53,6 +66,10 @@ public class CidadeController {
     }
 
     @ApiOperation("Atualiza uma cidade específica com base no ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Cidade atualizada"),
+            @ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+                    content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class)))})
     @PutMapping("/{cidadeId}")
     public CidadeModel atualizar(@ApiParam(value = "ID de uma cidade") @PathVariable Long cidadeId, @ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados.") @RequestBody @Valid CidadeInput cidadeInput) {
         try {
@@ -65,6 +82,10 @@ public class CidadeController {
     }
 
     @ApiOperation("Exclui uma cidade específica com base no ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Cidade excluída"),
+            @ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+                    content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class)))})
     @DeleteMapping("/{cidadeId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@ApiParam(value = "ID de uma cidade") @PathVariable Long cidadeId) {
