66bb745b59eba0cf1b3d18fc09df6f6440bf07b5
CarlosEReis <carlos.er7@gmail.com>
2024-04-17 23:48:56 -0300

    18.22: Desafio: descrevendo documentação de endpoints de cozinhas

--- src/main/java/com/er7/er7foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/er7/er7foodapi/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import com.er7.er7foodapi.api.assembler.CozinhaInputDisassembler;
 import com.er7.er7foodapi.api.assembler.CozinhaModelAssembler;
 import com.er7.er7foodapi.api.model.CozinhaModel;
 import com.er7.er7foodapi.api.model.input.CozinhaInput;
+import com.er7.er7foodapi.api.openapi.controller.CozinhaControllerOpenApi;
 import com.er7.er7foodapi.domain.model.Cozinha;
 import com.er7.er7foodapi.domain.repository.CozinhaRepository;
 import com.er7.er7foodapi.domain.service.CadastroCozinhaService;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,40 +22,40 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cozinhas")
-public class CozinhaController {
+public class CozinhaController implements CozinhaControllerOpenApi {
 
     @Autowired private CozinhaRepository cozinhaRepository;
     @Autowired private CadastroCozinhaService cadastroCozinha;
     @Autowired private CozinhaModelAssembler cozinhaModelAssembler;
     @Autowired private CozinhaInputDisassembler cozinhaInputDisassembler;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
         Page<Cozinha> pageCozinhas = cozinhaRepository.findAll(pageable);
         List<CozinhaModel> listCozinhasModel = cozinhaModelAssembler.toColletionModel(pageCozinhas.getContent());
         return new PageImpl<>(listCozinhasModel, pageable, pageCozinhas.getTotalElements());
     }
 
-    @GetMapping("/{cozinhaId}")
+    @GetMapping(path = "/{cozinhaId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public CozinhaModel buscar(@PathVariable Long cozinhaId) {
         return cozinhaModelAssembler.toModel(cadastroCozinha.buscaOuFalha(cozinhaId));
     }
 
-    @PostMapping
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public CozinhaModel adicionar(@RequestBody @Valid CozinhaInput cozinhaInput) {
         var cozinha = cozinhaInputDisassembler.toDomainObject(cozinhaInput);
         return cozinhaModelAssembler.toModel(cadastroCozinha.salvar(cozinha));
     }
 
-    @PutMapping("/{cozinhaId}")
+    @PutMapping(path = "/{cozinhaId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public CozinhaModel atualizar(@PathVariable Long cozinhaId, @RequestBody @Valid CozinhaInput cozinhaInput) {
         var cozinhzaDB = this.cadastroCozinha.buscaOuFalha(cozinhaId);
         cozinhaInputDisassembler.copyToDomainObject(cozinhaInput, cozinhzaDB);
         return cozinhaModelAssembler.toModel(cadastroCozinha.salvar(cozinhzaDB));
     }
 
-    @DeleteMapping("/{cozinhaId}")
+    @DeleteMapping(path = "/{cozinhaId}", produces = {})
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long cozinhaId) {
         this.cadastroCozinha.excluir(cozinhaId);
---
+++ src/main/java/com/er7/er7foodapi/api/openapi/controller/CozinhaControllerOpenApi.java
@@ -0,0 +1,47 @@
+package com.er7.er7foodapi.api.openapi.controller;
+
+import com.er7.er7foodapi.api.exceptionhandler.Problem;
+import com.er7.er7foodapi.api.model.CozinhaModel;
+import com.er7.er7foodapi.api.model.input.CozinhaInput;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+
+@Api(tags = "Cozinhas")
+public interface CozinhaControllerOpenApi {
+
+    @ApiOperation("Lista as cozinhas com páginação.")
+    public Page<CozinhaModel> listar(Pageable pageable);
+
+    @ApiOperation("Busca uma cozinha por ID.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "404", description = "Cozinha não encontrada TESTE.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Problem.class))),
+        @ApiResponse(responseCode = "400", description = "ID da cozinha inválido.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Problem.class))) })
+    public CozinhaModel buscar(@ApiParam(value = "ID de uma cozinha", example = "1") Long cozinhaId);
+
+    @ApiOperation("Cadastra uma cozinha.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Cozinha cadastrada.") })
+    public CozinhaModel adicionar(@ApiParam(value = "Representação do modelo de entrada de uma nova cozinha.") CozinhaInput cozinhaInput);
+
+    @ApiOperation("Atualiza uma cozinha por ID.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Cozinha Atualizada", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Problem.class))),
+        @ApiResponse(responseCode = "404", description = "Cozinha não encontrada.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Problem.class))) })
+    public CozinhaModel atualizar(
+            @ApiParam(value = "ID de uma cozinha", example = "1") Long cozinhaId,
+            @ApiParam(name = "corpo", value = "Representação de uma cozinha com os novos dados") CozinhaInput cozinhaInput);
+
+    @ApiOperation("Exclui uma cozinha por ID.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "204", description = "Cozinha excluída", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Problem.class))),
+        @ApiResponse(responseCode = "404", description = "Cozinha não encontrada.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Problem.class))) })
+    public void remover(@ApiParam(value = "ID de uma cozinha", example = "1") Long cozinhaId);
+}
