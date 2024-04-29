9641554c95a2edcb4481106bb91719a76b11179c
CarlosEReis <carlos.er7@gmail.com>
2024-04-29 19:23:05 -0300

    18.39: Desafio: descrevendo documentação de endpoints de associação de grupos com usuários

--- src/main/java/com/er7/er7foodapi/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/er7/er7foodapi/api/controller/UsuarioGrupoController.java
@@ -2,6 +2,7 @@ package com.er7.er7foodapi.api.controller;
 
 import com.er7.er7foodapi.api.assembler.GrupoModelAssembler;
 import com.er7.er7foodapi.api.model.GrupoModel;
+import com.er7.er7foodapi.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.er7.er7foodapi.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios/{usuarioID}/grupos")
-public class UsuarioGrupoController {
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired private CadastroUsuarioService usuarioService;
     @Autowired private GrupoModelAssembler grupoModelAssembler;
@@ -21,15 +22,17 @@ public class UsuarioGrupoController {
         return grupoModelAssembler.toCollectionModel(usuarioService.listarGrupos(usuarioID));
     }
 
+    @Override
     @DeleteMapping("/{grupoID}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void desassocionar(@PathVariable Long usuarioID, @PathVariable Long grupoID) {
+    public void desassociar(@PathVariable Long usuarioID, @PathVariable Long grupoID) {
         usuarioService.desassociaGrupo(usuarioID, grupoID);
     }
 
+    @Override
     @PutMapping("/{grupoID}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void associonar(@PathVariable Long usuarioID, @PathVariable Long grupoID) {
+    public void associar(@PathVariable Long usuarioID, @PathVariable Long grupoID) {
         usuarioService.associoaGrupo(usuarioID, grupoID);
     }
 
--- src/main/java/com/er7/er7foodapi/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
+++ src/main/java/com/er7/er7foodapi/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
@@ -2,6 +2,7 @@ package com.er7.er7foodapi.api.openapi.controller;
 
 import com.er7.er7foodapi.api.exceptionhandler.Problem;
 import com.er7.er7foodapi.api.model.FormaPagamentoModel;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -11,26 +12,24 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import java.util.List;
 
+
+@Api(tags = "Restaurantes")
 public interface RestauranteFormaPagamentoControllerOpenApi {
 
     @ApiOperation("Lista as formas de pagamento associadas a restaurante")
     @ApiResponses({
-            @ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class)) )
-    })
+            @ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class))) })
     public List<FormaPagamentoModel> listar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteID);
-
 
     @ApiOperation("Desassociação de restaurante com forma de pagamento")
     @ApiResponses({
             @ApiResponse(responseCode = "204", description = "Desassociação realizada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Restaurante ou forma de pagamento não encontrado", content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class)))
-    })
+            @ApiResponse(responseCode = "404", description = "Restaurante ou forma de pagamento não encontrado", content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class))) })
     public void desassociar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteID, @ApiParam(value = "ID da forma de pagamento", example = "1", required = true) Long formaPagamentoID);
 
     @ApiOperation("Associação de restaurante com forma de pagamento")
     @ApiResponses({
             @ApiResponse(responseCode = "204", description = "Associação realizada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Restaurante ou forma de pagamento não encontrado", content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class)))
-    })
+            @ApiResponse(responseCode = "404", description = "Restaurante ou forma de pagamento não encontrado", content = @Content(mediaType = "application/json",  schema = @Schema(implementation = Problem.class))) })
     public void associar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteID, @ApiParam(value = "ID da forma de pagamento", example = "1", required = true) Long formaPagamentoID);
 }
