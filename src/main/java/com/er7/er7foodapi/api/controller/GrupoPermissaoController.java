147d8a13f0560014926a70afeddb6f1c74843c8d
CarlosEReis <carlos.er7@gmail.com>
2024-04-29 19:06:56 -0300

    18.37: Desafio: descrevendo documentação de endpoints de associação de permissões com grupos

--- src/main/java/com/er7/er7foodapi/api/controller/GrupoPermissaoController.java
+++ src/main/java/com/er7/er7foodapi/api/controller/GrupoPermissaoController.java
@@ -3,34 +3,43 @@ package com.er7.er7foodapi.api.controller;
 
 import com.er7.er7foodapi.api.assembler.PermissaoModelAssembler;
 import com.er7.er7foodapi.api.model.PermissaoModel;
+import com.er7.er7foodapi.api.openapi.controller.GrupoPermissaoControllerOpenApi;
+import com.er7.er7foodapi.domain.model.Grupo;
 import com.er7.er7foodapi.domain.service.CadastroGrupoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/grupos/{grupoID}/permissoes")
-public class GrupoPermissaoController {
+public class GrupoPermissaoController implements GrupoPermissaoControllerOpenApi {
 
     @Autowired private PermissaoModelAssembler permissaoModelAssembler;
     @Autowired private CadastroGrupoService cadastroGrupoService;
 
-    @GetMapping
-    public List<PermissaoModel> permissoes(@PathVariable Long grupoID) {
-        return permissaoModelAssembler.toColletionModel(cadastroGrupoService.permissoes(grupoID));
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PermissaoModel> listar(@PathVariable Long grupoId) {
+        Grupo grupo = cadastroGrupoService.buscarOuFalhar(grupoId);
+        return permissaoModelAssembler.toColletionModel(grupo.getPermissoes());
     }
 
+    @Override
     @PutMapping("/{permissaoID}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void associar(@PathVariable Long grupoID, @PathVariable Long permissaoID) {
         cadastroGrupoService.associar(grupoID, permissaoID);
     }
 
+    @Override
     @DeleteMapping("/{permissaoID}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void desassociar(@PathVariable Long grupoID, @PathVariable Long permissaoID) {
         cadastroGrupoService.desassociarPermissao(grupoID, permissaoID);
     }
+
+
 }
